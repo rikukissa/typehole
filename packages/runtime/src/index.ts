92b1a12ccbedd095a155efbd65b6b2792f7f8f02
Riku Rouvila <riku.rouvila@gmail.com>
2021-05-05 20:03:02 +0300

    send collected samples directly from runtime to extension
    extension now handles transforming the values into types
    
    this is to pave way for multiple samples

--- packages/runtime/src/index.ts
+++ packages/runtime/src/index.ts
@@ -2,24 +2,53 @@ import fetch from "isomorphic-fetch";
 
 let queue: Promise<any> = Promise.resolve();
 
+function serialize(value: any) {
+  let serialized: string | null = null;
+  try {
+    serialized = JSON.stringify(value);
+  } catch (error) {}
+  return serialized;
+}
+
+function isPlainObject(value: any) {
+  return typeof value === "object" && value.toString() === "[object Object]";
+}
+
 function typeholeFactory(id: string | symbol | number) {
   return function typehole<T = any>(input: T): T {
-    try {
+    const serialized = serialize(input);
+
+    if (!serialized || (serialized === "{}" && !isPlainObject(input))) {
       queue = queue.then(() =>
-        fetch("http://localhost:17341/samples", {
+        fetch("http://localhost:17341/unserializable", {
           method: "POST",
           mode: "cors",
           body: JSON.stringify({
             id,
-            sample: input,
           }),
           headers: {
             "Content-Type": "application/json",
           },
         }).catch((err) => console.log(err.message))
       );
-    } catch (error) {
-      console.error(error);
+    } else {
+      try {
+        queue = queue.then(() =>
+          fetch("http://localhost:17341/samples", {
+            method: "POST",
+            mode: "cors",
+            body: JSON.stringify({
+              id,
+              sample: input,
+            }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }).catch((err) => console.log(err.message))
+        );
+      } catch (error) {
+        console.error(error);
+      }
     }
 
     return input;
